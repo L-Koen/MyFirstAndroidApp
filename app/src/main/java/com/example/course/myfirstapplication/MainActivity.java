0e29d002e236090727cf62bd41017bf0f3522940
L-Koen <121684425+L-Koen@users.noreply.github.com>
2023-12-22 17:03:42 +0100

    Created my first app

---
+++ app/src/main/java/com/example/course/myfirstapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.course.myfirstapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView helloTextView = (TextView) findViewById(R.id.text_view_1);
+        helloTextView.setText(R.string.final_text);
+    }
+}
